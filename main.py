587642d0ec8b05c8702c866e8e1a24946e4eba8d
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-02-09 13:00:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,21 @@
 import check_input
 import maze
 
-def read_maze():
+def read_maze(): #put maze file
   maze = []
-  with open( , 'r') as f:
+  with open( , 'r') as f: # put maze file
     for line in f:
       row = list(line.rstrip(\n'))
                  if row:
                              maze.append(row)
   return maze                          
+
+
+
+def find_start(maze):
+  for r in range(len(maze)):
+    for c in range(len(maze[r])):
+      in maze[r][c] == 's'
+        return [r, c]
+  return None
+  
