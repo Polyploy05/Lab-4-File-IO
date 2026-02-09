3557d38571cd2a2f4a1edd07591626b2e90f2ed0
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-02-09 12:56:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-### Name:
+''' Name: Jacob Miranda
 ### Date:
 ### Group:
+### Description:  '''
 
+import check_input
+import maze
+
+def read_maze():
+  maze = []
+  with open( , 'r') as f:
+    for line in f:
+      row = list(line.rstrip(\n'))
+                 if row:
+                             maze.append(row)
+  return maze                          
